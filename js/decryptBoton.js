70bc164d88a88c74c0b9d705cc0763ae6a622cd1
Adolfo Carrillo <gustavoadolfocarrillocamacho@gmail.com>
2022-01-19 18:47:43 -0600

    Implementing the logic of encryption and decryption to the buttons

---
+++ js/decryptBoton.js
@@ -0,0 +1,25 @@
+var decryptBoton = document.querySelector("#decryptBoton");
+
+decryptBoton.addEventListener("click", function (event) {
+ 
+    event.preventDefault();
+
+    let text = document.querySelector("#messageToProcess");
+
+    let messageToProcess = text.value;
+
+    let message = decrypter(messageToProcess);
+
+    if (messageToProcess == "") {
+        alert("Type a message to decript");
+    } else {
+        console.log("Decripted Message: ", message);
+
+        let messageProcessed = document.querySelector("#messageProcessed");
+
+        messageProcessed.value = "Decrypted Message: " + message;
+
+        //messageProcessed.value = message;
+    }
+    text.value = "";
+})
